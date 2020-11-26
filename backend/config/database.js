1c08e079a53072c6870eb4138a20307baff2e3d4
Dmitry Matiouchenko <dmitrymatio@gmail.com>
2020-11-26 02:23:45 -0800

    strapi db config test settings

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,14 +5,22 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: 'public',
       },
       options: {
         ssl: false,
+        pool: {
+          "min": 0,
+          "max": 15,
+          "idleTimeoutMillis": 30000,
+          "createTimeoutMillis": 30000,
+          "acquireTimeoutMillis": 30000
+        },
       },
     },
   },
